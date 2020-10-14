3ed83ecf089d4751a2ee33a9507f192e43d07bd6
Neil Dino <N.dino90@gmail.com>
2020-10-13 21:07:50 -0600

    pages render when clicked. Nav css to show current page

--- src/App.js
+++ src/App.js
@@ -1,21 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 import Nav from './components/Nav'
 import About from './components/About'
 import ContactForm from './components/Contact';
-import Portfolio from './components/Portfolio'
+import Portfolio from './components/Portfolio';
+import Resume from './components/Resume'
 
 function App() {
+  const [content] = useState(["about", "portfolio", "contactForm", "resume"]);
+  const [contentSelected, setContentSelected]= useState(content[0])
+
   return (
     <div className="App">
       <div>
-        <Nav></Nav>
+        <Nav contentSelected={contentSelected}
+        setContentSelected={setContentSelected}/>
       </div>
       <main>
-        <About></About>
-        <Portfolio></Portfolio>
-        <ContactForm></ContactForm>
+        {(contentSelected === "about") && <About/>}
+        {(contentSelected === "portfolio") && <Portfolio/>}
+        {(contentSelected === "contactForm") && <ContactForm/>}
+        {(contentSelected === "resume") && <Resume/>}
       </main>
     </div>
   );
---
+++ src/components/Resume/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Resume(){
+    return(
+        <div className="paper">Resume Will go here Wih a download link</div>
+    )
+}
+
+export default Resume;
